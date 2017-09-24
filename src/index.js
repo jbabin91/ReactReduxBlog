10155854cf6caa9d694d56b972a7f5f91a41ff05
Jace Babin <jbabin91@gmail.com>
2017-09-23 17:05:16 -0700

    Added react router to project.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/style.css'
+
+// Components
 import App from './components/app'
+
+// Reducers
 import reducers from './reducers'
+
+// ServiceWorker
+import registerServiceWorker from './registerServiceWorker'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
+class Hello extends React.Component {
+  render () { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render () { return <div>Goodbye!</div> }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={App} />
+        <Route path='/hello' component={Hello} />
+        <Route path='/goodbye' component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>, document.getElementById('root'))
 registerServiceWorker()
