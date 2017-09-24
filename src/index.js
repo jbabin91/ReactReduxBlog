d88df04e68f4bfa927ec2a7e2d13b6d2d6d21a7f
Jace Babin <jbabin91@gmail.com>
2017-09-23 17:25:45 -0700

    Removed test routes and added a posts index and setup an index route

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/style.css'
 
-// Components
-import App from './components/app'
+// Import Components
+import PostsIndex from './components/posts_index'
 
 // Reducers
 import reducers from './reducers'
@@ -19,21 +19,11 @@ import registerServiceWorker from './registerServiceWorker'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
-class Hello extends React.Component {
-  render () { return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-  render () { return <div>Goodbye!</div> }
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={App} />
-        <Route path='/hello' component={Hello} />
-        <Route path='/goodbye' component={Goodbye} />
+        <Route path='/' component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>, document.getElementById('root'))
